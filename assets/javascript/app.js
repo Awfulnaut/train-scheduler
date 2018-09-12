059f82245b6e90439e4f101a2125272ab7cea489
Cadin McQueen <cadin.mcqueen@digitalwave.com>
2018-09-11 20:47:36 -0400

    Initial commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyBbIwhl49SI9jrYIPQFxTTGbJb-Lr5izSg",
+  authDomain: "train-scheduler-9d838.firebaseapp.com",
+  databaseURL: "https://train-scheduler-9d838.firebaseio.com",
+  projectId: "train-scheduler-9d838",
+  storageBucket: "train-scheduler-9d838.appspot.com",
+  messagingSenderId: "786650510950"
+};
+firebase.initializeApp(config);
