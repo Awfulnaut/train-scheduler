314151d4d6f506d28739dab8623e61d426a134b3
Cadin McQueen <cadin.mcqueen@digitalwave.com>
2018-09-13 21:40:47 -0400

    Add front-end markup and styles

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,4 +7,34 @@ var config = {
   storageBucket: "train-scheduler-9d838.appspot.com",
   messagingSenderId: "786650510950"
 };
+
 firebase.initializeApp(config);
+
+var database = firebase.database();
+
+$(document).ready(function() {
+  $("#add-train-btn").on("click", function(event) {
+    event.preventDefault();
+
+    var trainName = $('#train-name-input').val().trim();
+    var destination = $('#destination-input').val().trim();
+    var time = moment($('#time-input').val().trim(), "HH:mm").format("X");
+    console.log(time);
+    var frequency = $('#frequency-input').val().trim();
+  })
+});
+
+
+
+// Initialize page with placeholder? train info
+
+// Accept user input
+  // Train name, destination, First train time, and frequency (and submit button)
+
+// Calculate when the next train will arrive, and how many minutes away it is (taking first train time and frequency into account)
+
+// Submit button click event
+  // Clear input values
+  // Calculations
+  // push data to the database
+  // Refresh/push changes to the DOM
