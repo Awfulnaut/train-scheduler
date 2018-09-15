930688612bc1af65e8ebea3b191da6ed306bab23
Cadin McQueen <cadin.mcqueen@digitalwave.com>
2018-09-15 17:15:49 -0400

    Add time calculation logic

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,34 +7,60 @@ var config = {
   storageBucket: "train-scheduler-9d838.appspot.com",
   messagingSenderId: "786650510950"
 };
-
 firebase.initializeApp(config);
-
 var database = firebase.database();
 
 $(document).ready(function() {
   $("#add-train-btn").on("click", function(event) {
     event.preventDefault();
 
+    // Store user input in variables
     var trainName = $('#train-name-input').val().trim();
-    var destination = $('#destination-input').val().trim();
-    var time = moment($('#time-input').val().trim(), "HH:mm").format("X");
-    console.log(time);
-    var frequency = $('#frequency-input').val().trim();
-  })
+    var trainDest = $('#destination-input').val().trim();
+    var trainTime = $('#time-input').val().trim();
+    var trainFrequency = $('#frequency-input').val().trim();
+
+    // Map values submitted from the form to an object
+    var newTrain = {
+      name: trainName,
+      destination: trainDest,
+      time: trainTime,
+      frequency: trainFrequency
+    }
+  
+    // Push object to Firebase
+    database.ref().push(newTrain);
+  
+    // Clear out text boxes
+    $('#train-name-input').val("");
+    $('#destination-input').val("");
+    $('#time-input').val("");
+    $('#frequency-input').val("");
+  });
+
+  database.ref().on("child_added", function(snapshot) {
+    var trainName = snapshot.val().name;
+    var trainDest = snapshot.val().destination;
+    var trainTime = snapshot.val().time;
+    var trainFrequency = snapshot.val().frequency;
+  
+    // Calculate train's next arrival time and minutes away from the current time
+    firstArrivalTime = moment(trainTime, "HH:mm").subtract(1, "years");
+    var diffTime = moment().diff(moment(firstArrivalTime), "minutes");
+    var timeRemainder = diffTime % trainFrequency;
+    var minutesAway = trainFrequency - timeRemainder;
+    var nextArrival = moment().add(minutesAway, "minutes");
+  
+    // Append table data elements containing the values to a new row
+    var newRow = $("<tr>").append(
+      $("<td>").text(trainName),
+      $("<td>").text(trainDest),
+      $("<td>").text(trainFrequency),
+      $("<td>").text(moment(nextArrival).format("hh:mm")),
+      $("<td>").text(minutesAway)
+    );
+  
+    // Append the new row to the table
+    $("#train-table > tbody").append(newRow);
+  });
 });
-
-
-
-// Initialize page with placeholder? train info
-
-// Accept user input
-  // Train name, destination, First train time, and frequency (and submit button)
-
-// Calculate when the next train will arrive, and how many minutes away it is (taking first train time and frequency into account)
-
-// Submit button click event
-  // Clear input values
-  // Calculations
-  // push data to the database
-  // Refresh/push changes to the DOM
